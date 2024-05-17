d9d285e9f5e6aca43c05034c3b8c9f33c7ddc631
David-Chemaly <davidchemaly@hotmail.com>
2024-05-17 11:04:11 +0100

    loaded all the data

--- get_data.py
+++ get_data.py
@@ -14,13 +14,6 @@ from scipy.stats import truncnorm
 from skimage.util import view_as_windows
 
 def slidding_window(arr, window_shape=(2, 224, 224)):
-    # # Ensure the dimensions are divisible by the window shape
-    # x_shape, y_shape = arr.shape
-    # x_size = window_shape[0] * ( x_shape // window_shape[0] )
-    # y_size = window_shape[1] * ( y_shape // window_shape[1] )
-
-    # corrected_arr = arr[:x_size, :y_size]
-
     # Use view_as_windows to slice the array into windows
     windows = view_as_windows(arr, window_shape, window_shape)
 
@@ -35,13 +28,13 @@ def slidding_window(arr, window_shape=(2, 224, 224)):
 
 def dwarf_segmentation(data, header, host_filter, RA, DEC, ID):
 
-    # Get the pixel corners of the image
-    x_pixels, y_pixels = data.shape
-
     # Get dwarfs coordinates in pixels
     w = wcs.WCS(header)
     dwarf_x_pixel, dwarf_y_pixel = np.round( w.wcs_world2pix(RA, DEC, 0) ).astype(int)
-    dwarf_x_pixel, dwarf_y_pixel, id_dwarfs = dwarf_x_pixel[host_filter], dwarf_y_pixel[host_filter], ID[host_filter]
+    if index == 128:
+        dwarf_x_pixel, dwarf_y_pixel = dwarf_x_pixel[host_filter][1:], dwarf_y_pixel[host_filter][1:]
+    else:
+        dwarf_x_pixel, dwarf_y_pixel = dwarf_x_pixel[host_filter], dwarf_y_pixel[host_filter]
 
     dwarf_segmentation = np.zeros_like(data, dtype=int)
     for i in range(len(dwarf_x_pixel)):
@@ -50,6 +43,15 @@ def dwarf_segmentation(data, header, host_filter, RA, DEC, ID):
     return dwarf_segmentation
 
 
+def adjust_duplicates(array, arg_TP):
+    seen = set(arg_TP)
+    for index, value in enumerate(array):
+        while value in seen:
+            value += 1  # Adjust incrementally
+        seen.add(value)
+        array[index] = value
+    return array
+    
 if __name__ == '__main__':
     PATH_DATA = '/Volumes/ES-LaCie/Documents/MATLAS'
     PATH_SAVE = '/Volumes/ES-LaCie/Documents/MATLAS/Data/r_band'
@@ -57,17 +59,8 @@ if __name__ == '__main__':
     # Load the Galaxies in MATLAS
     galaxies = np.loadtxt(f'{PATH_DATA}/galaxies.txt', dtype=str)
 
-    # Correct between the names in the catalog and the names in the images
-    real_galaxies_missing = np.array(['IC0719', 'NGC3400', 'NGC3499', 'NGC3613', 'NGC3630', 'NGC4249', 'NGC4255', 'NGC5173', 'NGC5353', 'NGC5473', 'NGC5839'])
-    real_corresponding_tiles = np.array(['IC0719', 'NGC3414', 'NGC3499', 'NGC3619', 'NGC3640', 'NGC4259', 'NGC4255', 'NGC5198', 'NGC5355', 'NGC5485', 'NGC5845'])
-    real_galaxies_to_tile = dict(zip(real_galaxies_missing, real_corresponding_tiles))
-
-    for i in range(len(galaxies)):
-        if galaxies[i] in real_galaxies_missing:
-            galaxies[i] = real_galaxies_to_tile[galaxies[i]]
-    np.savetxt(f'{PATH_DATA}/galaxies.txt', galaxies, fmt='%s')
-    
     for index in tqdm(range(len(galaxies)), leave=True):
+
         # Load r-band image
         with fits.open(f'{PATH_DATA}/{galaxies[index]}.l.r.Mg004.fits') as hdul:
             primary_hdu = hdul[0]
@@ -79,7 +72,7 @@ if __name__ == '__main__':
         ra_dwarf, dec_dwarf, id_dwarfs, host = np.array(df['RAJ2000']), np.array( df['DEJ2000']), np.array( df['ID']), np.array( df['HostETG'])
 
         catalogue_wrong = np.array(['NGC0525', 'NGC2594', 'NGC2699', 'NGC3665', 'NGC4251', 'NGC7465'])
-        catalogue_right = np.array(['NGC0524', 'NGC2592', 'NGC2695', 'NGC3658', 'NGC4251', 'NGC7457'])
+        catalogue_right = np.array(['NGC0524', 'NGC2592', 'NGC2695', 'NGC3658', 'NGC4251', 'NGC7454'])
         wrong_to_right = dict(zip(catalogue_wrong, catalogue_right))
 
         for i in range(len(host)):
@@ -107,21 +100,9 @@ if __name__ == '__main__':
             f.create_dataset("data",  data=windows[arg_TP, 0])
             f.create_dataset("label", data=xy_label)
 
-        NN = 1000
-        lower_bound, upper_bound = 0, len(windows)
-        mean, std_dev = len(windows)/2, len(windows)/5
-        a, b = (lower_bound - mean) / std_dev, (upper_bound - mean) / std_dev
-        int_samples = truncnorm.rvs(a, b, loc=mean, scale=std_dev, size=NN).astype(int)
-
-        def adjust_duplicates(array, arg_TP):
-            seen = set(arg_TP)
-            for index, value in enumerate(array):
-                while value in seen:
-                    value += 1  # Adjust incrementally
-                seen.add(value)
-                array[index] = value
-            return array
-
-        arg_TN = adjust_duplicates(int_samples, arg_TP)
+        NN = 500
+        arg_TN = np.where(windows[:, 1].sum(axis=(1,2)) == 0)[0]
+        TN_chosen = windows[np.random.choice(arg_TN, NN, replace=False), 0]
+        TN_save   = TN_chosen[TN_chosen.sum(axis=(1,2)) != 0]
         with h5py.File(f'{PATH_SAVE}/{galaxies[index]}/TN.h5', 'w') as f:
-            f.create_dataset("data",  data=windows[arg_TN, 0])
+            f.create_dataset("data",  data=TN_save)
